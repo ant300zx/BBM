2bf0352a64861c04e357bbefd765424d4f723155
Anthony Cull <acull76@gmail.com>
2014-07-31 15:07:16 +0100

    Added BBM for the first time

---
+++ js/app.js
@@ -0,0 +1,74 @@
+var MyApp = new Backbone.Marionette.Application();
+
+MyApp.addRegions({
+  mainRegion: "#clientTable"
+});
+
+MyApp.addInitializer(function() {
+	var multiUsersCollection = new MultiUsersCollection();
+	var usersCompositeView = new UsersCompositeView({
+		collection: multiUsersCollection
+	});
+
+	MyApp.mainRegion.show(usersCompositeView);
+	multiUsersCollection.fetch();
+	
+});
+
+//User model
+var UserModel = Backbone.Model.extend({
+	url: "delete"
+});
+
+//user collection
+var MultiUsersCollection = Backbone.Collection.extend({
+	model: UserModel,
+	url: "js/users.json",
+
+	initialize: function() {}
+});
+
+//User view
+var UserItemView = Backbone.Marionette.ItemView.extend({
+	template: Handlebars.compile($("#userTemplate").html()),
+	tagName: "tr",
+	className: "singleUser",
+
+	events: {
+		"click a.delete": "deleteUser"
+	},
+
+	deleteUser: function() {
+		this.model.destroy();
+	}
+});
+
+var UsersCompositeView = Backbone.Marionette.CompositeView.extend({
+
+	template: Handlebars.compile($("#usersTemplate").html()),
+	tagName: "form",
+	attributes: {
+		action: "",
+		method: "POST"
+	},
+	itemView: UserItemView,
+	itemViewContainer: 'tbody',
+
+	events: {
+		"click .add": "addUser"
+	},
+
+	addUser: function(e) {
+		
+		this.collection.add({ 	
+			firstname: this.el.newFirstName.value,
+			lastname: this.el.newLastName.value
+		});
+
+		this.el.reset();
+		e.preventDefault();
+
+	}
+});
+
+MyApp.start();
